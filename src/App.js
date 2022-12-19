c492977d1ee6a7bbc397053ed3822df454693f01
KingsukDhar101 <kingsuk.1997dhar2@gmail.com>
2022-12-19 20:11:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { marked } from "marked";
+import { sampleData } from "./data";
 
 function App() {
+  const [text, setText] = useState(sampleData);
+
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = "0px";
+      const scrollHeight = textAreaRef.current.scrollHeight;
+      textAreaRef.current.style.height = scrollHeight + "px";
+    }
+  }, [textAreaRef.current, text]);
+
+  marked.setOptions({
+    breaks: true,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="box">
+          <div className="editor_title">Editor</div>
+          <textarea
+            name=""
+            id="editor"
+            value={text}
+            ref={textAreaRef}
+            rows={1}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          ></textarea>
+        </div>
+        <div className="box">
+          <div className="previewer_title">Previewer</div>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{
+              __html: marked(text),
+            }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
